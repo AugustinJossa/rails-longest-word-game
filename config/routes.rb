e7e5a890adf1d9b25a05f48c69c32b40674d0e92
Augustin Jossa <augustin.jossa@hec.edu>
2017-08-08 09:08:52 +0200

    Ex2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # get 'playing/game'
+  get 'game', to: 'playing#game'
+  get 'score', to: 'playing#score'
+  # get 'playing/score'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
